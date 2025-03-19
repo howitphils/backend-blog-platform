5b7d296143db5f822377c177e4f720e055c2340b
howitphils <uladzislauhr@gmail.com>
2025-03-19 20:01:54 +0300

    removed validating blogId

--- src/middlewares/posts-body-validators/posts-validators.ts
+++ src/middlewares/posts-body-validators/posts-validators.ts
@@ -29,23 +29,23 @@ const validateContent = body("content")
   .isLength({ max: 1000 })
   .withMessage("Length is more than 1000 symbols");
 
-const validateBlogId = body("blogId")
-  .isString()
-  .withMessage("Should be a string")
-  .notEmpty()
-  .withMessage("Should not be empty");
-// .custom(async (blogId: string) => {
-//   const convertedId = new ObjectId(blogId);
-//   const targetBlog = await blogsRepository.getBlogById(convertedId);
-//   if (!targetBlog) {
-//     throw new Error("Blog does not exist");
-//   }
-//   return true;
-// });
+// const validateBlogId = body("blogId")
+//   .isString()
+//   .withMessage("Should be a string")
+//   .notEmpty()
+//   .withMessage("Should not be empty");
+// // .custom(async (blogId: string) => {
+// //   const convertedId = new ObjectId(blogId);
+// //   const targetBlog = await blogsRepository.getBlogById(convertedId);
+// //   if (!targetBlog) {
+// //     throw new Error("Blog does not exist");
+// //   }
+// //   return true;
+// // });
 
 export const postsBodyValidator = [
   validateTitle,
   validateShortDescription,
   validateContent,
-  validateBlogId,
+  // validateBlogId,
 ];
