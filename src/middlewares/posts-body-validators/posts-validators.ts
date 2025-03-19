93cab0ae5d55bf13589f001b0c1ce7d7a7f7b2ea
howitphils <uladzislauhr@gmail.com>
2025-03-19 20:00:00 +0300

    removed custom validator for blogId

--- src/middlewares/posts-body-validators/posts-validators.ts
+++ src/middlewares/posts-body-validators/posts-validators.ts
@@ -33,15 +33,15 @@ const validateBlogId = body("blogId")
   .isString()
   .withMessage("Should be a string")
   .notEmpty()
-  .withMessage("Should not be empty")
-  .custom(async (blogId: string) => {
-    const convertedId = new ObjectId(blogId);
-    const targetBlog = await blogsRepository.getBlogById(convertedId);
-    if (!targetBlog) {
-      throw new Error("Blog does not exist");
-    }
-    return true;
-  });
+  .withMessage("Should not be empty");
+// .custom(async (blogId: string) => {
+//   const convertedId = new ObjectId(blogId);
+//   const targetBlog = await blogsRepository.getBlogById(convertedId);
+//   if (!targetBlog) {
+//     throw new Error("Blog does not exist");
+//   }
+//   return true;
+// });
 
 export const postsBodyValidator = [
   validateTitle,
