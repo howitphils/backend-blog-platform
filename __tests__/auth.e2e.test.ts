a8be55d0d7d291b6bc3eb58f2e28b13f2a395b1a
howitphils <uladzislauhr@gmail.com>
2025-04-01 17:01:08 +0300

    added tests for login

---
+++ __tests__/auth.e2e.test.ts
@@ -0,0 +1,49 @@
+import { MongoClient } from "mongodb";
+import { runDb } from "../src/db/mongodb/mongodb";
+import { SETTINGS } from "../src/settings";
+import { req } from "./test-helpers";
+import { encodedCredentials } from "../src/middlewares/auth-validator";
+
+describe("/auth", () => {
+  let client: MongoClient;
+
+  beforeAll(async () => {
+    // Создаем новое тестовое соединение
+    client = await runDb(SETTINGS.MONGO_URL, SETTINGS.TEST_DB_NAME);
+
+    const res = await req
+      .post(SETTINGS.PATHS.USERS)
+      .set("Authorization", `Basic ${encodedCredentials}`)
+      .send({
+        login: "new-user",
+        password: "string",
+        email: "example@example.com",
+      });
+
+    expect(res.status).toBe(201);
+  });
+
+  afterAll(async () => {
+    // Закрываем коннект с дб
+    await client.close();
+    console.log("Connection closed");
+  });
+
+  it("it should login a correct user", async () => {
+    const res = await req.post(SETTINGS.PATHS.AUTH + "/login").send({
+      loginOrEmail: "new-user",
+      password: "string",
+    });
+
+    expect(res.status).toBe(204);
+  });
+
+  it("it should not login an incorrect user", async () => {
+    const res = await req.post(SETTINGS.PATHS.AUTH + "/login").send({
+      loginOrEmail: "incorrect",
+      password: "string",
+    });
+
+    expect(res.status).toBe(401);
+  });
+});
